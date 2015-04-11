6a5b773689f494a19188a2a2c2726e2c9da13ca2
GulliverJ <gully.johnson@gmail.com>
2015-04-11 03:07:26 +0100

    setting up sensor adding functionality

--- sensor_manager.php
+++ sensor_manager.php
@@ -20,7 +20,7 @@
     // Retrieve the current max sensor_id belonging to this user
     $sql_statement = $connection->prepare("SELECT sensor_id FROM sensors WHERE operator_id = '" . $_SESSION['operator_id'] . "'ORDER BY sensor_id DESC LIMIT 1;");
     $sql_statement->execute();
-
+    
     $sensor_id = $sql_statement->fetchColumn();
     if(empty($sensor_id)) {
       $sensor_id = 1;
@@ -28,20 +28,21 @@
       $sensor_id = $sensor_id + 1;
     }
     echo $sensor_id;
+    /*
     // Add the sensor to the database:
     $sql_statement = $connection->prepare( "INSERT INTO sensors(operator_id, sensor_id, application, measures, unit, lat, lng) VALUES (?, ?, ?, ?, ?, ?, ?);" );
     $sql_statement->execute( array( $_SESSION['operator_id'], $sensor_id, $application, $measures, $unit, $lat, $lng));
-
+*/
     // At this point, the code should communicate to Java that a new sensor has been added.
     // The java code initialises the sensor in the Cassandra table, and once that is done it
     // sets the sensor as 'active' in this SQL table.
-    
+
     header("location:portal.php");
 
- }
- catch( PDOException $e )
- {
-   var_dump($e);
-   die( "There was an internal database error whilst creating your user, error code (" . $e->getCode() . ")" );
- }
+  }
+  catch( PDOException $e )
+  {
+    var_dump($e);
+    die( "There was an internal database error whilst creating your user, error code (" . $e->getCode() . ")" );
+  }
 ?>
