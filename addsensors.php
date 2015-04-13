477d6792b3c04c8d4794663b56a5fcd1f31b5e1c
GulliverJ <gully.johnson@gmail.com>
2015-04-13 02:26:48 +0100

    Testing google map location input

--- addsensors.php
+++ addsensors.php
@@ -11,72 +11,65 @@
     #map {
       width: 100%;
       height: 500px;
+      margin: 16px;
     }
   </style>
   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false"></script>
   <script>
 
-function initialize() {
-  var myLatLng = new google.maps.LatLng(51.524636, -0.132036);
+    function initialize() {
+      var myLatLng = new google.maps.LatLng(51.524636, -0.132036);
 
-  var mapOptions = {
-    zoom: 15,
-    center: myLatLng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+      var mapOptions = {
+        zoom: 15,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-  var map = new google.maps.Map(document.getElementById('map'),
-      mapOptions);
+      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Set lat/lon values for this property',
-    draggable: true
-  });
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Set lat/lon values for this property',
+        draggable: true
+      });
 
-  google.maps.event.addListener(map, 'zoom_changed', function() {
-    var zoomLevel = map.getZoom();
-    map.setCenter(myLatLng);
-    infowindow.setContent('Zoom: ' + zoomLevel);
-  });
+      google.maps.event.addListener(map, 'zoom_changed', function() {
+        var zoomLevel = map.getZoom();
+        map.setCenter(myLatLng);
+        infowindow.setContent('Zoom: ' + zoomLevel);
+      });
 
-google.maps.event.addListener(map, "idle", function() {
+      google.maps.event.addListener(map, "idle", function() {
         marker.setPosition(map.getCenter());
         $("#latin").val(map.getCenter().lat().toFixed(6));
         $("#lngin").val(map.getCenter().lng().toFixed(6));
       });
+
       google.maps.event.addListener(marker, "dragend", function(mapEvent) {
         map.panTo(mapEvent.latLng);
       });
 
+      google.maps.event.addDomListener(window, 'load', initialize);
+    }
 
-google.maps.event.addListener(marker, 'dragend', function(a) {
-  document.getElementById("map-output").innerHTML = "Latitude:  " + map.getCenter().lat().toFixed(6) + "<br>Longitude: " + map.getCenter().lng().toFixed(6) + "<a href='https://www.google.com/maps?q=" + encodeURIComponent(map.getCenter().toUrlValue()) + "' target='_blank'>Go to maps.google.com</a>";
-  console.log(a);
-  // bingo!
-  // a.latLng contains the co-ordinates where the marker was dropped
-});
-
-google.maps.event.addDomListener(window, 'load', initialize);
-}
   </script>
 </head>
-
-
 <body onload="initialize()">
 
 <?php
-if(!LoggedIn()) { ?>
-    <h4>You must be logged in to add sensors. Click <a href="index.php">here</a> to log in.</h4><?php
+if(!LoggedIn()) {
+?>
+  <h4>You must be logged in to add sensors. Click <a href="index.php">here</a> to log in.</h4>
+<?php
 } else {
 ?>
 
-<h1>Add new sensors</h1>
-<h3>Log in!</h3>
+<h1>Add new sensors</h1><br>
 <form name="addsensor" method="post" action="sensor_manager.php" id="addsensor" novalidate>
   <label for="application">Sensor Application</label>
-  <input name="application" id="application" type="text" placeholder="Label your sensor"><br>
+  <input name="application" id="application" type="text" placeholder="Label your sensor"><br><br>
   <label for="measures">What does the sensor measure?</label>
   <select name="measures" id="measures" style="width: 350px">
     <option value="" selected disabled>Choose a property</option>
@@ -90,7 +83,7 @@ if(!LoggedIn()) { ?>
     <option value="rainfall">Rainfall</option>
     <option value="temperature">Temperature</option>
     <option value="windspeed">Windspeed</option>
-  </select><br>
+  </select><br><br>
   <!-- TODO: make this intelligent with JQuery -->
   <label for="unit">Which unit will the data have?</label>
   <select name="unit" id="unit" style="width: 350px">
@@ -107,19 +100,21 @@ if(!LoggedIn()) { ?>
     <option value="C">Celcius</option>
     <option value="F">Fahrenheit</option>
     <option value="K">Kelvin</option>
-  </select><br>
+  </select><br><br>
   <p>Where exactly will your sensor be positioned</p>
   <label for="lat">Latitude</label>
-  <input name="lat" id="latin" type="number" step="0.000001" placeholder="Lat" value = ""><br>
+  <input name="lat" id="latin" type="number" step="0.000001" placeholder="Lat" value = ""><br><br>
   <label for="lng">Longitude</label>
-  <input name="lng" id="lngin" type="number" step="0.000001" placeholder="Lng" value - ""><br>
-  <button type="submit" name="add">Register this sensor</button>
+  <input name="lng" id="lngin" type="number" step="0.000001" placeholder="Lng" value - ""><br><br>
+  <button type="submit" name="add">Register this sensor</button><br>
       <div id="map">
     </div>
 </form>
 
 <?php
-} ?>
+}
+?>
+
 <a href="index.php">Home</a>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </body>
