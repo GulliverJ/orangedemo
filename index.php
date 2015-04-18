7f62e8f7540d17e14715e64f7a9bd1969eaa620a
GulliverJ <gully.johnson@gmail.com>
2015-04-18 22:23:25 +0100

    Files

--- index.php
+++ index.php
@@ -54,6 +54,11 @@
           </div>
       </div>  
     </section>  
+
+    <div class="container-fluid footer">
+      <p style="float: right">This project is proudly presented in partnership with</p>
+    </div>
+
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
