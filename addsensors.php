18eeb171f8f7b95f2a99a729c11a8c8638930133
vnjk <vnjk@users.noreply.github.com>
2015-04-18 01:31:18 +0100

    updating addsensors.php

--- addsensors.php
+++ addsensors.php
@@ -4,7 +4,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Orange Sensors</title>
+   <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/styles.css">
+
+  
   <meta charset="utf-8">
   <style>
     #map {
@@ -54,8 +68,50 @@
     }
 
   </script>
+
+  <title>Add Sensors</title>
+
 </head>
 <body onload="initialize()">
+      <nav class="navbar navbar-default">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <?php
+                $sign = "SIGN IN";
+                $signlink = "#";
+                    if(LoggedIn()) { ?>
+                        <p class='navbar-text' style="color:#ffffff">Logged in as <?php echo GetUserName(); ?>.</p>
+                        <!--<a href="logout.php"><p class='navbar-text'>Log out</p></a>-->
+                        <?php
+                            $sign = "SIGN OUT";
+                            $signlink = "logout.php";
+                    }
+                    ?>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="main-menu">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="features.html">FEATURES</a></li>
+                <li class="active"><a href="<?php echo $signlink ?>"><?php echo $sign ?></a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">CONTACT US</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
+ <section class="content">
+        <div class="container">
+-----------------------------------
 
 <?php
 if(LoggedIn()) {
@@ -117,6 +173,21 @@ if(LoggedIn()) {
 <?php
 }
 ?>
+
+
+  </div>
+</section>
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
 </body>
 </html>
