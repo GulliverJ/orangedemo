d0ff81604de0de05f33d679e8bea971d86e3345b
vnjk <vnjk@users.noreply.github.com>
2015-04-20 20:41:57 +0100

    updating test

--- portal.php
+++ portal.php
@@ -140,12 +140,16 @@ if(LoggedIn()) { ?>
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-    <script>
+
+
+     <script>
     $.getJSON('http://orange-peel.herokuapp.com/sensors/find/11' + id, function(data) {
         console.log(data);
     });
     </script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
 </body>
 </html>
